d0badc1e53378ed43633341184045df8859f641c
Yappaholic <sav.boyar@gmail.com>
2024-05-03 14:40:45 +0300

    Make players swap each round

--- script.ts
+++ script.ts
@@ -2,7 +2,7 @@ const array: any = document.createElement("p")
 const body = document.querySelector("body")
 const makeBoard = () => {
   let size = 3;
-  let board = [];
+  let board: any = [];
   for (let i = 0; i < size; i++) {
     board[i] = [];
     for (let j = 0; j < size; j++) {
@@ -33,36 +33,37 @@ const createPlayers = (playerName: string, token: number) => {
 };
 const askMove = () => {
   let ask = prompt("What's your move?")
-  let arr = ask.split(",")
+  let arr: any = ask?.split(",")
   playerOne.makeMove(Number(arr[0]), Number(arr[1]))
 };
 const checkWin = () => {
-  for (let i = 0; i < board.board.length; i++) {
-    let rowSum = board.board[i].reduce((a: number, b: number) => Number(a) + Number(b))
+  let boardi = board.board;
+  for (let i = 0; i < boardi.length; i++) {
+    let rowSum = boardi[i].reduce((a: number, b: number) => Number(a) + Number(b))
     if (rowSum == 3) {
       console.log(`${playerOne.playerName} win!`)
     } else if (rowSum == 6) {
       console.log(`${playerTwo.playerName} win!`)
     }
   }
-  let obj = {}
-  for (let j = 0; j < board.board.length; j++) {
-    for (let x = 0; x < board.board[j].length; x++) {
-      if (!obj[board.board[j][x]]) {
-        let token = board.board[j][x];
-        obj[token] = 1;
-      } else if (obj[board.board[j][x]]) {
-        let token = board.board[j][x];
-        obj[token]++
-        if (obj[token] === 3) {
-          return "You win!"
-        }
-      }
+  for (let j = 0; j < boardi.length; j++) {
+    let colSum = Number(boardi[0][j]) + Number(boardi[1][j]) + Number(boardi[2][j])
+    if (colSum === 3) {
+      console.log(`${playerOne.playerName} win!`)
+    } else if (colSum === 6) {
+      console.log(`${playerTwo.playerName} win!`)
     }
+  }
+  let rightDiagonal = Number(boardi[0][0]) + Number(boardi[1][1]) + Number(boardi[2][2]);
+  let leftDiagonal = Number(boardi[0][2]) + Number(boardi[1][1]) + Number(boardi[2][1]);
+  if (rightDiagonal === 3 || leftDiagonal === 3) {
+    console.log(`${playerOne.playerName} win!`)
+  } else if (rightDiagonal === 6 || leftDiagonal === 6) {
+    console.log(`${playerTwo.playerName} win!`)
   }
 }
 const playerOne = createPlayers("PlayerOne", 1);
 const playerTwo = createPlayers("PlayerTwo", 2);
 const board = makeBoard();
 askMove()
-body.appendChild(array)
+body?.appendChild(array)
