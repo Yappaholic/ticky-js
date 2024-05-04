b7381a33422ab37bd5c87cbbd5826d0d50b4fd02
Yappaholic <sav.boyar@gmail.com>
2024-05-04 15:39:30 +0300

    Add event listeners to cells

--- script.ts
+++ script.ts
@@ -12,7 +12,7 @@ const makeBoard = () => {
   function getBoard() { return board; }
   return { board, getBoard };
 };
-const createPlayers = (playerName: string, token: number) => {
+const createPlayers = (playerName: string, token: "X" | "O") => {
   function getName() { return playerName; }
   function getToken() { return token; }
   const makeMove = (row: number, column: number) => {
@@ -31,39 +31,53 @@ const createPlayers = (playerName: string, token: number) => {
 
   return { playerName, token, getName, getToken, makeMove };
 };
+let count = 1;
 const askMove = () => {
   let ask = prompt("What's your move?")
   let arr: any = ask?.split(",")
-  playerOne.makeMove(Number(arr[0]), Number(arr[1]))
+  if (count % 2 === 1) {
+    playerOne.makeMove(Number(arr[0]), Number(arr[1]));
+    count++;
+  } else if (count % 2 === 0) {
+    playerTwo.makeMove(Number(arr[0]), Number(arr[1]));
+    count++;
+  }
 };
-const checkWin = () => {
+const checkWin = (): boolean => {
   let boardi = board.board;
   for (let i = 0; i < boardi.length; i++) {
-    let rowSum = boardi[i].reduce((a: number, b: number) => Number(a) + Number(b))
-    if (rowSum == 3) {
+    let rowSum = boardi[i].reduce((a: string, b: string) => a + b);
+    if (rowSum == "XXX") {
       console.log(`${playerOne.playerName} win!`)
-    } else if (rowSum == 6) {
+      return true;
+    } else if (rowSum == "OOO") {
       console.log(`${playerTwo.playerName} win!`)
+      return true;
     }
   }
   for (let j = 0; j < boardi.length; j++) {
-    let colSum = Number(boardi[0][j]) + Number(boardi[1][j]) + Number(boardi[2][j])
-    if (colSum === 3) {
+    let colSum = (boardi[0][j]) + (boardi[1][j]) + (boardi[2][j])
+    if (colSum === "XXX") {
       console.log(`${playerOne.playerName} win!`)
-    } else if (colSum === 6) {
+      return true;
+    } else if (colSum === "OOO") {
       console.log(`${playerTwo.playerName} win!`)
+      return true;
     }
   }
-  let rightDiagonal = Number(boardi[0][0]) + Number(boardi[1][1]) + Number(boardi[2][2]);
-  let leftDiagonal = Number(boardi[0][2]) + Number(boardi[1][1]) + Number(boardi[2][1]);
-  if (rightDiagonal === 3 || leftDiagonal === 3) {
+  let rightDiagonal = (boardi[0][0]) + (boardi[1][1]) + (boardi[2][2]);
+  let leftDiagonal = (boardi[0][2]) + (boardi[1][1]) + (boardi[2][1]);
+  if (rightDiagonal === "XXX" || leftDiagonal === "XXX") {
     console.log(`${playerOne.playerName} win!`)
-  } else if (rightDiagonal === 6 || leftDiagonal === 6) {
+    return true;
+  } else if (rightDiagonal === "OOO" || leftDiagonal === "OOO") {
     console.log(`${playerTwo.playerName} win!`)
+    return true;
   }
+  return false;
 }
-const playerOne = createPlayers("PlayerOne", 1);
-const playerTwo = createPlayers("PlayerTwo", 2);
+const playerOne = createPlayers("PlayerOne", "X");
+const playerTwo = createPlayers("PlayerTwo", "O");
 const board = makeBoard();
 askMove()
 body?.appendChild(array)
